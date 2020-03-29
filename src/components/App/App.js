cac8c9a8364044612092c6571aa737cead50d713
Jeffrey Goodall <goodalls@live.com>
2020-03-29 10:40:33 -0600

    Adding working React Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,51 @@
 import React from 'react';
-
-import Navigation from "../Navigation/Navigation";
-
-
 import './App.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Home from '../Home/Home';
+import About from '../About/About';
+import Services from '../Services/Services';
+import FAQ from '../FAQ/FAQ';
+import Contact from '../Contact/Contact';
+import { Switch, Route, Link } from "react-router-dom";
 
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       <table className="table1">
         <tbody>
           <Header />
-          <Navigation />
-          <Home />
+          <div className="Navigation">
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/ABOUT'>ABOUT ME</Link></li>
+              <li><Link to='/SERVICES'>SERVICES</Link></li>
+              <li><Link to='/FAQ'>FAQ</Link></li>
+              <li><Link to='/CONTACT'>CONTACT ME</Link></li>
+            </ul>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/services">
+                <Services />
+              </Route>
+              <Route path="/FAQ">
+                <FAQ />
+              </Route>
+              <Route path="/Contact">
+                <Contact />
+              </Route>
+            </Switch>
+          </div>
           <Footer />
-
         </tbody>
-      </table>
-
-
-    </div>
+      </table >
+    </div >
   );
 }
 
-export default App;
+
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -9,34 +9,11 @@ import {
 export default function () {
     return (
         <div className="Navigation">
-            <tr bgcolor="#330000">
-                <td height="33">&nbsp;</td>
-                <td height="33">
-                    <table className="tableTwo">
-                        <tbody>
-                            <tr>
-                                <td width="13%">
-                                    <div align="center"><a href="index.htm" class="style4">HOME</a></div>
-                                </td>
-                                <td width="23%">
-                                    <div align="center"><a href="about.htm" class="style4">ABOUT ME</a></div>
-                                </td>
-                                <td width="22%">
-                                    <div align="center"><a href="services.htm" class="style4">SERVICES</a></div>
-                                </td>
-                                <td width="12%">
-                                    <div align="center"><a href="faq.htm" class="style4">FAQ</a></div>
-                                </td>
-                                <td width="30%">
-                                    <div align="center"><a href="contact.htm" class="style4">CONTACT ME</a></div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>
-                <td height="33">&nbsp;</td>
-            </tr>
-
-        </div>
+            <div align="center"><a href="index.htm" class="style4">HOME</a></div>
+            <div align="center"><a href="about.htm" class="style4">ABOUT ME</a></div>
+            <div align="center"><a href="services.htm" class="style4">SERVICES</a></div>
+            <div align="center"><a href="faq.htm" class="style4">FAQ</a></div>
+            <div align="center"><a href="contact.htm" class="style4">CONTACT ME</a></div>
+        </div> /*NAVIGATION*/
     )
 };
