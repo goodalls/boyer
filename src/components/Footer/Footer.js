b940d677c5bdd9f65aa551677143b8c19a3e28ed
Jeffrey Goodall <goodalls@live.com>
2020-03-29 11:08:37 -0600

    refactor footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,9 @@ import "./Footer.scss";
 export default function Footer() {
     return (
         <div className="Footer">
-            <div class="style5" align="center">&copy; 2020 Cami Boyer&nbsp;&nbsp;&nbsp;<img src={swirl} align="absmiddle" width="60" />&nbsp;&nbsp;&nbsp;Licensed Professional Counselor, Licensed Addiction Counselor, Master Addictions Counselor</div>
+            <div class="style5" align="center">
+                &copy; 2020 Cami Boyer&nbsp;&nbsp;&nbsp;<img src={swirl} align="absmiddle" width="60" />&nbsp;&nbsp;&nbsp;Licensed Professional Counselor, Licensed Addiction Counselor, Master Addictions Counselor
+            </div>
         </div>
     )
 };
